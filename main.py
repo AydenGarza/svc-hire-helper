89d02caae9ab9b6e8fa423c34d3436a2aedd7a9b
Ayden Garza <aydenjgarza@gmail.com>
2026-05-09 02:06:00 -0500

    Added supabase connection?

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from supabase import create_client, Client
 import os
 
 load_dotenv()
@@ -9,6 +10,13 @@ SUPABASE_KEY = os.getenv("SUPABASE_SECRET")
 
 if not SUPABASE_KEY or not SUPABASE_URL:
 	raise Exception("Supabase credentials were not loaded from environment")
+
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+response = (supabase.table("applications").select("*").execute())
+
+print(response)
 
 app = FastAPI()
 
