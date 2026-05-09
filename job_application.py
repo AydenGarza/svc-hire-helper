d2362095ec3044c357b1dda74dcfca15958cf8e2
Ayden Garza <aydenjgarza@gmail.com>
2026-05-09 15:27:34 -0500

    working post to db

--- job_application.py
+++ job_application.py
@@ -1,10 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
 
-class job_application(BaseModel):
+class JobApplication(BaseModel):
 	company: str
 	job_title:str
 	date_applied:str
 	application_status:str
-	application_id:str #primary key in sb db
-	uid:str
-	
+	username:str
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, Depends
 from supabase import Client
 from db import get_db
+from job_application import JobApplication 
 
 app = FastAPI()
 
 @app.get("/db_test")
 def get_data(db = Depends(get_db)):
 	response = db.table("applications").select("*").execute()
+	return response
+
+@app.post("/create_application")
+def create_application(application: JobApplication, db = Depends(get_db)):
+	print("this happens")
+	response = (db.table("applications").insert(application.model_dump()).execute())
 	return response
